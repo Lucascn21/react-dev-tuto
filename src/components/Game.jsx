eda2d0b177259ec80b6820a82ee525b75f3933ea
lucascn21 <lucascn21@gmail.com>
2023-04-01 20:44:33 -0300

    Display “You are at move #…” on current move

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -2,31 +2,37 @@ import { useState } from "react";
 import { Board } from "./Board";
 
 export const Game = () => {
-    const [history, setHistory] = useState([Array(9).fill(null)]);
-    const [currentMove, setCurrentMove] = useState(0);
-    const xIsNext = currentMove % 2 === 0;
-    const currentSquares = history[currentMove];
-  
-    function handlePlay(nextSquares) {
-      const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-      setHistory(nextHistory);
-      setCurrentMove(nextHistory.length - 1);
-    }
-  
-    function jumpTo(nextMove) {
-      setCurrentMove(nextMove);
-    }
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
 
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) {
+    if (move > 0 && move < history.length - 1) {
       description = "Go to move #" + move;
+    } else if (move == currentMove) {
+      description = "You are at move #" + Number(currentMove + 1);
     } else {
       description = "Go to game start";
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {move == currentMove ? (
+          <p>{description}</p>
+        ) : (
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        )}
       </li>
     );
   });
